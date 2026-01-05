f0281f73f16bf86e6f86518cbaa31b6950dc7d95
Venugopal <mailmevenugopal.a@gmail.com>
2026-01-05 22:05:41 +0530

    5thJan

---
+++ ex1.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+console.log("This is JavaScript");
+
+let x = 100;
+console.log(x);
+
+var y = 12.45;
+console.log(y);
+
+let st = "Frontend Online Training";
+console.log(st);
+
+let ch = true;
+console.log(ch);
